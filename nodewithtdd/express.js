64749e3f2179c8137a97e129c8af8918f8d31421
jindaram-stu <jinsb1999@naver.com>
2022-12-29 23:25:12 +0900

    feat: express를 활용한 서버 및 미들웨어 사용

---
+++ nodewithtdd/express.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan')
+
+app.use(morgan('dev'))
+
+app.listen(3000, () => {
+    console.log('Server is running');
+});
+
