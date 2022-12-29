cbf732342a3fd8d5d2d2cae557815f3103c10753
jindaram-stu <jinsb1999@naver.com>
2022-12-29 23:24:52 +0900

    feat: http 모듈을 활용한 서버

--- nodewithtdd/helloworld.js
+++ nodewithtdd/helloworld.js
@@ -4,9 +4,19 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World \n');
+
+    if (req.url === '/') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Hello World \n');
+    }
+
+    if (req.url === '/user') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('user route');
+    }
+    
 });
 
 server.listen(port, hostname, () => {
